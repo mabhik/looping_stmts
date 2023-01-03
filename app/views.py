95fdf42c26fa9e89fbbb16f4a77a7dee9f07d691
mabhikumar <mabhikumar66@gmail.com>
2023-01-03 07:56:14 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def forloop(request):
     a={'abi':'abhi','a':[1,2,3,4,5,6,7]}
     return render(request,'forloop.html',a)
+
+def loop(request):
+    b={'jsp':'institution','brnch':'marathahalli'}
+    return render(request,'loop.html',b)
--- project12/urls.py
+++ project12/urls.py
@@ -19,5 +19,6 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('forloop/',forloop,name='forloop')
+    path('forloop/',forloop,name='forloop'),
+    path('loop/',loop,name='loop')
 ]
